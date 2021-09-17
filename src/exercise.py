4a2c3f5c56b092dffc7260c3e335f9389b9161be
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 06:02:32 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -2,10 +2,24 @@ def main():
     #write your code below this line
     
     year = int(input("Give a year:"))
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        print("The year is a leap year.")
+    
+#     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+#         print("The year is a leap year.")
+#     else:
+#         print("The year is not a leap year.")
+
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+            leap = False
+        leap = True
+        
+    if leap:
+        print('The year is a leap year.')
     else:
-        print("The year is not a leap year.")
+        print('The year is not a leap year.')
+                
         
 if __name__ == '__main__':
     main()
